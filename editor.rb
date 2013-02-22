12c96a2ca45d72bfe8b4a57308d6031c87bd95e9
ruslashev <ruslashev@gmail.com>
2013-02-22 23:23:59 +0400

    Proper textboxes and better command mode

--- editor.rb
+++ editor.rb
@@ -1,44 +1,55 @@
 def main_loop
-  Ncurses.raw
-  Ncurses.nonl
-  Ncurses.keypad(Ncurses.stdscr, true)
+  Ncurses.cbreak
   Ncurses.noecho
+  Ncurses.keypad(Ncurses.stdscr, true)
 
   begin
     yield
   ensure
     Ncurses.echo
-    Ncurses.nocbreak
-    Ncurses.nl
-    Ncurses.curs_set(1)
     Ncurses.endwin
   end
 end
 
-def textBox y, x, scr
-  command = ""
-  cx = 0
-  scr.move(y, x)
-  scr.refresh
+def textBox y, x, scr, background = false, text = "$ ", max_len = (scr.getmaxx-x-text.length-1)
+  string = ""
+  cursx = 0
 
   loop do
-    case ch = scr.getch
-      when 32..126
-        command.insert(cx, ch.chr)
-        scr.mvprintw(y, x, command)
-        cx += 1 if cx < [command.length, Ncurses.getmaxx(scr)].min
-      when KEY_RIGHT
-        cx += 1 if cx < [command.length, Ncurses.getmaxx(scr)].min
-      when KEY_LEFT
-        cx = [x, cx-1].max
-      when KEY_ENTER
-        return command
-        break
-
+    if background
+      scr.attron(A_REVERSE)
+      scr.mvprintw(Ncurses.getmaxy(scr)-1, 0, "-"*(Ncurses.getmaxx(scr)))
+      scr.attroff(A_REVERSE)
     end
-      scr.move(y, x+cx)
-      scr.refresh
-  end
+    scr.mvaddstr(y, x, text)
+    scr.mvaddstr(y, x+text.length, string)
+    scr.move(y, x+text.length+cursx)
+    ch = scr.getch
+    case ch
+    when KEY_LEFT
+      cursx = [0, cursx-1].max
+    when KEY_RIGHT
+      cursx = [max_len, cursx+1, string.length].min
+    when KEY_ENTER, KEY_EOL, KEY_EOS, ?\r, ?\n, 10
+      return string
+    when KEY_BACKSPACE, 263
+      string = string[0..-2]
+      cursx = [0, cursx-1].max
+    when KEY_DL, 330
+      if cursx < string.length
+        unless cursx == 0
+          string = string[0..cursx-1]+string[(cursx == 0 ? 0 : cursx+1)..-1]
+        else
+          string = string[1..-1]
+        end
+      end
+    when 32..126
+      if string.length <= max_len
+        string.insert(cursx, ch.chr)
+        cursx = [max_len, cursx+1].min
+      end
+    end
+  end     
 end
 
 class Editor
@@ -54,23 +65,22 @@ class Editor
     @insert_mode = true # oh geezus please forgive my soul
   end
 
-  def redraw
+  def update
     @cur_llen = @file[cy].length
 
     @cx = [@cx, @cur_llen].min
+  end
 
+  def redraw
     @scr.clear
-    
     @file.each do |line|
       @scr.mvprintw(@file.index(line), 0, line)
     end
     
-    #@scr.attron(A_REVERSE)
+    @scr.attron(A_REVERSE)
     @scr.mvaddstr(@cy, @cx, "@")
-    #@scr.mvprintw(Ncurses.getmaxy(scr)-1, 0, "-"*(Ncurses.getmaxx(scr)))
-    @scr.mvprintw(Ncurses.getmaxy(scr)-1, 0, "INSERT") if @insert_mode
-    #@scr.attroff(A_REVERSE)
-
+    @scr.mvprintw(Ncurses.getmaxy(@scr)-1, 0, "-"*(Ncurses.getmaxx(@scr)))
+    @scr.attroff(A_REVERSE)
     @scr.refresh
   end
 
--- sprivling.rb
+++ sprivling.rb
@@ -12,35 +12,47 @@ main_loop do
   editor.redraw
 
   while (ch = scr.getch)
-    case editor.insert_mode
-      when true
-        case ch
-          when KEY_RIGHT
-            if editor.cx < [editor.cur_llen, Ncurses.getmaxx(scr)-1].min
-              editor.cx += 1
-            else
-              editor.move_down true
-            end
-          when KEY_LEFT
-            if editor.cx > 0
-              editor.cx -= 1
-            else
-              editor.move_up true
-            end
-          when KEY_DOWN
-            editor.move_down
-          when KEY_UP
-            editor.move_up
-          when 32..126
-            editor.file[editor.cy].insert(editor.cx, ch.chr)
-            editor.cx += 1
-          when 27 # escape
-            editor.insert_mode = ! editor.insert_mode
-        end
-      editor.redraw
-
-      when false
-        textBox(Ncurses.getmaxy(scr)-1, 2, scr)
+    case ch
+    when KEY_RIGHT
+      if editor.cx < [editor.cur_llen, Ncurses.getmaxx(scr)-1].min
+        editor.cx += 1
+      else
+        editor.move_down true
+      end
+    when KEY_LEFT
+      if editor.cx > 0
+        editor.cx -= 1
+      else
+        editor.move_up true
+      end
+    when KEY_DOWN
+      editor.move_down
+    when KEY_UP
+      editor.move_up
+    when 32..126
+      editor.file[editor.cy].insert(editor.cx, ch.chr)
+      editor.cx += 1
+    when KEY_BACKSPACE
+      editor.file[editor.cy] = editor.file[editor.cy][0..-2]
+      editor.cx = [0, editor.cx-1].max
+      awfawgh
+    when 27 # escape
+      editor.insert_mode = ! editor.insert_mode
     end
+    editor.update
+
+    if !editor.insert_mode
+      case textBox(Ncurses.getmaxy(scr)-1, 1, scr, true)
+        when "q"
+          break
+        else
+          editor.insert_mode = true
+      end
+    end
+    editor.redraw
+
+    scr.attron(A_REVERSE)
+    scr.mvprintw(Ncurses.getmaxy(scr)-1, 0, "INSERT") if editor.insert_mode
+    scr.attroff(A_REVERSE)
   end
 end
